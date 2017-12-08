ac9ac567879c305b4cb366f4006d211bf86825eb
Kyle Easton <kyleeaston@Kyles-MacBook-Air.local>
2017-12-08 10:59:47 -0800

    map added to page

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
+var gMaps = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg";
+
 $(document).ready(function() {
   console.log("Let's get coding!");
   // CODE IN HERE!
@@ -22,11 +24,37 @@ function onSuccess (potato) {
   for (let i = 0; i < potato.features.length; i ++) {
     if (potato.features[i].properties.mag >= 4.0) {
       let time = (Math.round((Date.now() - potato.features[i].properties.time) / 60 / 60 / 1000));
-        // Math.round(time);
       $("#info").append(`<p>${potato.features[i].properties.title} / ${time} hours ago</p>`);
     };
   };
 }
+
+function initMap() {
+
+map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: 37.78, lng: -122.44},
+  zoom: 5
+});
+
+
+
+}
+
+
+// $.ajax({
+//   method: "GET",
+//   url: gMaps,
+//   dataType: "json",
+//   success: mapsSuccess,
+//   error: function googleError() {
+//     console.log("maps not working. boo.");
+//   }
+// });
+
+// function mapSuccess(mapper) {
+//   console.log(mapper);
+
+// };
 
 // .features["0"].properties.title
 //.features["0"].properties.mag
