4f0e2c246e78b9a29e50e246fe4a26700a3f39dd
Kyle Easton <kyleeaston@Kyles-MacBook-Air.local>
2017-12-08 10:21:51 -0800

    js changes for time with title

--- scripts/app.js
+++ scripts/app.js
@@ -16,18 +16,24 @@ $.ajax({
 });
 
 function onSuccess (potato) {
+  // debugger; 
+  // using the word "debugger" will pause the js execution and let you interact with the variables that occurred at that time
   console.log(potato);
   for (let i = 0; i < potato.features.length; i ++) {
     if (potato.features[i].properties.mag >= 4.0) {
-      $('#info').append(`<p>${potato.features[i].properties.title}</p>`);
+      let time = (Math.round((Date.now() - potato.features[i].properties.time) / 60 / 60 / 1000));
+        // Math.round(time);
+      $("#info").append(`<p>${potato.features[i].properties.title} / ${time} hours ago</p>`);
     };
   };
 }
 
 // .features["0"].properties.title
 //.features["0"].properties.mag
+//new Date(1512756200*1000).toString()
+//"Fri Dec 08 2017 10:03:20 GMT-0800 (PST)"
 
-
+// let time = (Date.now() - (potato.features[i].properties.time) / 60 / 60 / 1000
 
 
 
